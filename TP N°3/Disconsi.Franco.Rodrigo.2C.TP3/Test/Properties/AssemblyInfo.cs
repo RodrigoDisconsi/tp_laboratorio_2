cba3a9643d28f9cb3334292585ea9a9fbbe891eb
rodrigodisconsi@gmail.com <RodrigoDisconsi@gmail.com>
2019-11-09 00:10:28 -0300

    TP N°3 Terminado.

---
+++ TP N°3/Disconsi.Franco.Rodrigo.2C.TP3/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f07970b2-1fae-4ca1-90ca-810dc8764102")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TP N°3/Disconsi.Franco.Rodrigo.2C.TP3/Test/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Excepciones;
+using EntidadesAbstractas;
+using Clases_Instanciables;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(DniInvalidoException))]
+
+        public void TestDniInvalidoException()
+        {
+            Alumno alumno = new Alumno(1, "Rodri", "Perez", "23234sadas", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NacionalidadInvalidaException))]
+
+        public void TestNacionalidadInvalidaException()
+        {
+            Alumno alumno = new Alumno(1, "Rodri", "Perez", "99999999", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+        }
+
+        [TestMethod]
+
+        public void TestDniEsperado()
+        {
+            string dni = "1111";
+            int esperado = 1111;
+            Alumno alumno = new Alumno(1, "Rodri", "Perez", dni, Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+            Assert.AreEqual(esperado, alumno.DNI);
+        }
+
+        [TestMethod]
+        public void TestAtributosNull()
+        {
+            Alumno alumno = new Alumno(1, "Rodri", "Perez", "1111", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+            Assert.IsNotNull(alumno.Nacionalidad);
+        }
+    }
+}
