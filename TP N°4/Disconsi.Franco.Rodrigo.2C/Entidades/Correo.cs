320cc001dc668e27749dea865f4bc3a666188028
rodrigodisconsi@gmail.com <RodrigoDisconsi@gmail.com>
2019-11-21 16:24:23 -0300

    TP 4 Empezado

--- TP N°4/Disconsi.Franco.Rodrigo.2C/Entidades/Correo.cs
+++ TP N°4/Disconsi.Franco.Rodrigo.2C/Entidades/Correo.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Entidades
 {
-    class Correro
+    public class Correo
     {
+        List<Thread> mockPaquetes;
+        List<Paquete> paquetes;
+
+        public List<Paquete> Paquetes
+        {
+            get
+            {
+                return this.paquetes;
+            }
+            set
+            {
+                this.paquetes = value;
+            }
+        }
+
+        public Correo()
+        {
+            this.mockPaquetes = new List<Thread>();
+            this.paquetes = new List<Paquete>();
+        }
     }
 }
