28f293f00d2c77fa6bf1fb3091316e182228ff67
rodrigodisconsi@gmail.com <RodrigoDisconsi@gmail.com>
2019-11-25 11:58:58 -0300

    TP N°4 Test

---
+++ TP N°4/Disconsi.Franco.Rodrigo.TP4/UnitTestProject1/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void ListPaqueteIsNotNull()
+        {
+            Correo correo = new Correo();
+
+
+            Assert.IsNotNull(correo.Paquetes);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TrackingIdRepetidoException))]
+        public void IdRepetido()
+        {
+            Correo correo = new Correo();
+            Paquete paquete1 = new Paquete("Quilmes", "1111");
+            Paquete paquete2 = new Paquete("Capital", "1111");
+
+            correo += paquete1;
+            correo += paquete2;
+        }
+    }
+}
